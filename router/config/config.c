311e0700d28f14cbb2c7777d703736391b68c3d5
qguan <qguan@localhost.localdomain>
2014-04-07 15:46:03 -0400

    add getRouter and writeRouter

--- router/config/config.c
+++ router/config/config.c
@@ -9,10 +9,151 @@ eth0_ip and eth0_ip_direct can not be used at the same time.
 **************/
 
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
+#include"libfile.h"
+#include"config.h"
 
-char i[80];
-char j[80];
+Router *getRouter(char *filename){
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    if ((fp = fopen(filename,"rt"))<0){
+        printf("getRouter: Faile to open file: %s \n", filename);
+        return;
+    }
+
+    Router *router;
+    //Ethernet ethx[ETHX];
+    router = (Router *)malloc(sizeof(Router));
+
+    char *tmp_param, *tmp_value, *saveptr;
+
+    int interface = -1; // the index to router->ethx[ETHX]
+    while ((read = getline(&line, &len, fp)) != -1) {
+        tmp_param = strtok_r(line, "=", &saveptr); // got the param before "="
+        tmp_value = strtok(saveptr, "#"); // ignore the comments
+
+        if(strstrip(tmp_param) == ""){
+            continue; // if blank line
+        }
+        if(strcmp(strstrip(tmp_param), "[EOF]") == 0){
+            break; // if end of file
+        }
+
+        if(strcmp(strstrip(tmp_param), "router_id") == 0){
+            snprintf(router->router_id, sizeof(router->router_id), "%s", strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "protocol_version") == 0){
+            snprintf(router->protocol_version, sizeof(router->protocol_version), "%s", strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "acquisition_authorization") == 0){
+            snprintf(router->acquisition_authorization, sizeof(router->acquisition_authorization), "%s", strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "hello_interval") == 0){
+            router->hello_interval = atoi(strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "ping_timeout") == 0){
+            router->ping_timeout = atoi(strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "ls_updated_interval") == 0){
+            router->ls_updated_interval = atoi(strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "ls_age_limit") == 0){
+            router->ls_age_limit = atoi(strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "hold_down_timer") == 0){
+            router->hold_down_timer = atoi(strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "num_of_interface") == 0){
+            router->num_of_interface = atoi(strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "eth_id") == 0){
+            interface++; // index start from 0
+            snprintf(router->ethx[interface].eth_id, sizeof(router->ethx[interface].eth_id), "%s", strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "direct_link_addr") == 0){
+            snprintf(router->ethx[interface].direct_link_addr, sizeof(router->ethx[interface].direct_link_addr), "%s", strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "link_cost_method") == 0){
+            snprintf(router->ethx[interface].link_cost_method, sizeof(router->ethx[interface].link_cost_method), "%s", strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "link_cost") == 0){
+            router->ethx[interface].link_cost = atoi(strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "link_failure_time") == 0){
+            router->ethx[interface].link_failure_time = atoi(strstrip(tmp_value));
+        }
+        if(strcmp(strstrip(tmp_param), "packet_error_rate") == 0){
+            router->ethx[interface].packet_error_rate = atoi(strstrip(tmp_value));
+        }
+
+    }
+    
+    return router;
+}
+
+int writeRouter(char *filename, Router *router){
+    copyFile(filename, LSRPCFGBK);
+
+    FILE *fp;
+
+    if ((fp = fopen(filename,"wt")) < 0){
+        printf("writeRouter: Failed to open file: %s\n", filename);
+        return -1;
+    }
+
+    char routerStr[2048];
+    memset(routerStr, 0, sizeof(routerStr));
+    snprintf(routerStr, sizeof(routerStr), "\
+# NOTE: 1) update ETHX in config.h if more interfaces defined here \n\
+#       2) value does NOT have \"\" even it's a string \n\
+\n\
+[Global config]\n\
+router_id = %s\n\
+protocol_version = %s\n\
+acquisition_authorization = %s\n\
+hello_interval = %d # in seconds\n\
+ping_timeout = %d # in seconds\n\
+ls_updated_interval = %d # in seconds\n\
+ls_age_limit = %d #in seconds\n\
+hold_down_timer = %d #in seconds\n\
+num_of_interface = %d\n\
+\n\
+[interface config]\n\
+", router->router_id, router->protocol_version, router->acquisition_authorization, \
+    router->hello_interval, router->ping_timeout, router->ls_updated_interval, \
+    router->ls_age_limit, router->hold_down_timer, router->num_of_interface);
+
+
+    /* generate the Ethernet part */
+    int i;
+    for(i=0;i<ETHX;i++){
+        char ethxStr[512];
+        memset(ethxStr, 0, sizeof(ethxStr));
+        snprintf(ethxStr, sizeof(ethxStr), "\
+[eth%d]\n\
+eth_id = %s\n\
+direct_link_addr = %s # remote host ip (router_id)\n\
+link_cost_method = %s # auto - calculated by  ping delay, manual - manual setting\n\
+link_cost = %d # infinit\n\
+link_failure_time = %d # seconds\n\
+packet_error_rate = %d\n\
+\n\
+", i, router->ethx[i].eth_id, router->ethx[i].direct_link_addr, router->ethx[i].link_cost_method, \
+    router->ethx[i].link_cost, router->ethx[i].link_failure_time, router->ethx[i].packet_error_rate);
+
+       strcat(routerStr, ethxStr); // add ethx information to router
+    }
+
+    fwrite(strstrip(routerStr), 1 , strlen(strstrip(routerStr)) , fp );
+
+    fclose(fp);
+    
+    return 0;
+}
 
 void cfgread(char filename[], char parameter[], char viarable[])
 {
@@ -139,9 +280,12 @@ void cfgwrite(char filename[], char parameter[], char content[])
 /*
 void main()
 {
-// cfgread("lsrp-router.cfg","link_cost_method",j);
-// printf("%s",j);
-   cfgwrite("lsrp-router.cfg","eth_0_id","10.66.10.3");
+//  cfgread("lsrp-router.cfg","link_cost_method",j);
+//  printf("%s",j);
+//  cfgwrite("lsrp-router.cfg","eth_0_id","10.66.10.3");
+
+    Router *router;
+    router = getRouter("lsrp-router.cfg");
+    writeRouter("lsrp-router.cfg", router);
 }
 */
-
--- router/config/config.h
+++ router/config/config.h
@@ -2,10 +2,41 @@
 #define __CONFIG_H__
 
 #define LSRPCFG "lsrp-router.cfg"
+#define LSRPCFGBK "lsrp-router.cfg.bk"
+#define ETHX 2 //define interfaces number
 
 char i[80];
 char j[80];
 
-void cfgread(FILE *fp, char parameter[], char viarable[]);
-void cfgwrite(FILE *fp, FILE *tempfile, char parameter[], char content[]);
+typedef struct Router_Ethernet{
+    char eth_id[16];// = 136.142.227.14
+    char direct_link_addr[16]; // = 136.142.227.15 // remote host ip (router_id)
+    char link_cost_method[32]; // = manual // auto - calculated by  ping delay, manual - manual setting
+    int link_cost; // = 9999 // infinit
+    int link_failure_time; // = 60 // seconds
+    int packet_error_rate; // = 0
+}Ethernet;
+
+typedef struct LSRP_Router{
+    /* [Global config] */
+    char router_id[16]; // = 127.0.0.1
+    char protocol_version[4]; // = 1.0
+    char acquisition_authorization[128]; //= password in md5sum
+    int hello_interval; // = 40 //seconds
+    int ping_timeout; // = 1 //seconds
+    int ls_updated_interval; // = 120 //seconds
+    int ls_age_limit; // = 60 //seconds
+    int hold_down_timer; // = 60 //seconds
+    int num_of_interface;// = 2
+    
+    /* [interface config] */
+    Ethernet ethx[ETHX]; // there will be more than one interfaces, it will be an interfaces array
+}Router;
+
+void cfgread(char filename[], char parameter[], char viarable[]);
+void cfgwrite(char filename[], char parameter[], char content[]);
+
+int writeRouter(char *filename, Router *router);
+Router *getRouter(char *filename);
+
 #endif
