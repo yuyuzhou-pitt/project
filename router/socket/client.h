5e8f4df546e914139648051b3bfb726ac1e8f7e7
qguan <qguan@localhost.localdomain>
2014-04-09 11:23:41 -0400

    head file for socket server and client

---
+++ router/socket/client.h
@@ -0,0 +1,6 @@
+#ifndef __CLIENT_H__
+#define __CLIENT_H__
+
+void *sockclient(void *arg);
+
+#endif
---
+++ router/socket/server.h
@@ -0,0 +1,7 @@
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+void *serverthread(void *arg);
+void *sockserver(void *arg);
+
+#endif
