a06dc8df87c9a9b4d349cd23a6f3600baa3f6527
qguan <qguan@localhost.localdomain>
2014-04-09 11:19:03 -0400

    update server.c as serverthread

--- router/socket/server.c
+++ router/socket/server.c
@@ -1,11 +1,7 @@
-/* Demo of Multi-thread version socket.
+/* Multi-thread version socket.
  *  - assign port automatically
  *  - timeout after given time
  *  - accept multi-client connection
- *
- * Compile and run:
- * $ gcc -g -pthread -o server server.c libsocket.c
- * ./server
 */
 
 #include <sys/types.h>
@@ -21,65 +17,103 @@
 #include <pthread.h>
 #include "libsocket.h"
 #include "getaddrinfo.h"
+#include "../packet/neighbor.h"
+#include "../packet/hello.h"
+#include "../config/config.h"
+#include "../config/liblog.h"
 
 #define PORT 0 //0 means assign a port randomly
 #define BUFFER_SIZE  1024
-#define MAX_QUE_CONN_NM   5
-#define TIMER  1000  //Timeout value in seconds
-#define CONTINUE     1 
+#define MAX_QUE_CONN_NM 5
+#define TIMER 1000  //Timeout value in seconds
+#define CONTINUE 1 
 #define NTHREADS 5
-#define MYIPADDR 136.142.227.15
+//#define LSRPCFG "config/lsrp-router.cfg"
 
 pthread_t threadid[NTHREADS]; // Thread pool
 pthread_mutex_t lock;
 int counter = 0;
 
+//char *lsrpcfg = "lsrp-router.cfg";
+char hostname[1024]; // local hostname and domain
+char addrstr[100]; // local ip address (eth0)
+int port; // local socket port
+
+
 /*thread for new client connection, arg is the client_fd*/
-void *sockthread(void *arg){
+void *serverthread(void *arg){
 
-    int sockfd; // File descriptor and 'read/write' to socket indicator
-    sockfd = (int) arg; // Getting sockfd from void arg passed in
+    /* Getting sockfd and router from void arg passed in */
+    ThreadParam *threadParam;
+    threadParam = (ThreadParam *)malloc(sizeof(ThreadParam));
+    threadParam = (ThreadParam *) arg;
+    int sockfd = threadParam->sockfd;
+    Router *router;
+    router = threadParam->router;
 
-    char buff[BUFFER_SIZE];
-    char buf[BUFFER_SIZE];
-    //neighbor_acq_msg *neighbor_req, *neighbor_cfm, *neighbor_rfs, *cease_req, *cease_cfm;
+    /* There are 5 types of Neighbor Acquisition Packets.
+     * Neighbor Acquisition Type:
+     * neighbor_req: Be_Neighbors_Request(000) or Cease_Neighbors_Request(100)
+     * neighbor_reply: Be_Neighbors_Confirm(101), Be_Neighbors_Refuse(111),
+     *                 or Cease_Neighbors_Confirm(101)   */ 
+    Packet *neighbor_req, *neighbor_reply; // MUST use pointer to fit different Packet
     
-    /* Receive from the remote side */
-    memset(buff, 0, sizeof(buff));
-    Recv(sockfd,buff,sizeof(buff),0);
-    //Recv(sockfd,neighbor_req,sizeof(neighbor_req),0);
+    /* Receive neighbors_req from remote side */
+    neighbor_req = (Packet *)malloc(sizeof(Packet));
+    Recv(sockfd, neighbor_req, sizeof(Packet), 0);
 
-    //neighbor_cfm = genCfm(neighbor_req);
+    /* generate neighbors_reply reply according to configure file */
+    neighbor_reply = genNeighborReq(router, addrstr, port); // msg to be sent back
+    genNeighborReply(router, neighbor_req, neighbor_reply); // update the Neighbor Acquisition Type
+    Send(sockfd, neighbor_reply, sizeof(Packet), 0);
 
-    /* confiem the request */
-    //if(neighbor_cfm.NeighborAcqType == 1)
-    /* Send to the remote side */
-    memset(buf , 0, sizeof(buf));
-    strcpy(buf,"return to client");
-    Send(sockfd,buf, sizeof(buf),0);
-  
+    char logmsg[128]; 
+    snprintf(logmsg, sizeof(logmsg), "serverthread(0x%x): reply neighbor acq type: %s\n", pthread_self(), neighbor_reply->Data.NeighborAcqType);
+    logging(LOGFILE, logmsg);
+
+    /* if neighbor request confirmed:
+     * 1) exchange alive (hello) message in HelloInterval seconds
+     * 2) exchange LSA message in UpdateInterval seconds, or every time there is updates
+     *     */
+
+    if(strcmp(neighbor_reply->Data.NeighborAcqType, "001") == 0){
+        /* use a thread to keep alive */
+        pthread_t hellothreadid;
+        pthread_create(&hellothreadid, NULL, &helloserver, (void *) sockfd);
+
+        /* use another thread for LSA */
+        //pthread_t LSAthreadid;
+        //pthread_create(&LSAthreadid, NULL, &LSAserver, (void *) sockfd);
+    }
+
+    sleep(3600);
+
     /* Critical section */
-    printf("Requesting mutex lock...\n");
     pthread_mutex_lock (&lock);
-    printf("Current counter value: %d, upping by 1...\n", counter);
     counter++;
     pthread_mutex_unlock (&lock);
-    printf("Done! Mutex unlocked again, new counter value: %d\n", counter);
   
     close(sockfd);
-    printf("TID:0x%x served request, exiting thread\n", pthread_self());
+    snprintf(logmsg, sizeof(logmsg), "serverthread(0x%x): served request, exiting thread\n", pthread_self());
+    logging(LOGFILE, logmsg);
+
     pthread_exit(0);
 
 }
 
-int main(){
+/* thread for lsrp-server main */
+void *sockserver(void *arg){
+    ThreadParam *threadParam;
+    threadParam = (ThreadParam *)malloc(sizeof(ThreadParam));
+    threadParam->router = (Router *)arg;
+
     struct sockaddr_in server_sockaddr, client_sockaddr;
     int sin_size, recvbytes, sendbytes;
     int sockfd, client_fd, desc_ready;
 
     sin_size=sizeof(client_sockaddr);
 
-    /*Data structure to handle timeout*/
+    /* Data structure to handle timeout */
     struct timeval before, timer, *tvptr;
     struct timezone tzp;
     
@@ -100,12 +134,9 @@ int main(){
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &i, sizeof(i));
     Bind(sockfd, server_sockaddr);
 
-    int port;
-    char str[6], hostname[1024], addrstr[100];
-    getaddr(hostname, addrstr); //get hostname and ip
+    getaddr(hostname, addrstr); //get hostname and ip, getaddrinfo.h
     port = Getsockname(sockfd, server_sockaddr, sin_size);  /* Get the port number assigned*/
-    sprintf(str, "%d", port); // int to str
-    writePort(str, addrstr);
+    writePort(port, addrstr);
     
     Listen(sockfd, MAX_QUE_CONN_NM);
    
@@ -132,6 +163,7 @@ int main(){
 
     int status;
     status=CONTINUE;
+    char logmsg[128];
     while (status==CONTINUE){
         if (iThread == NTHREADS){
             iThread = 0;
@@ -147,24 +179,28 @@ int main(){
                 break;
             case 0:
                 /* timeout occuired */
-                printf("\nTIMEOUT...\n");
+                logging(LOGFILE, "\nTIMEOUT...\n");
                 status=-1;
                 break;
             default:
                 if (FD_ISSET(sockfd, &ready_set)){
-                    printf("  Listening socket is readable\n");
+                    snprintf(logmsg, sizeof(logmsg), "sockserver: Listening socket is readable\n");
+                    logging(LOGFILE, logmsg);
                     /* wait for connection */
                     client_fd = Accept(sockfd, client_sockaddr, sin_size);
                     FD_SET(client_fd, &test_set);
                     if (client_fd > maxfd) maxfd = client_fd;
-                    printf("  Descriptor %d is readable\n", i);
+                    snprintf(logmsg, sizeof(logmsg), "sockserver: Descriptor %d is readable\n", i);
+                    logging(LOGFILE, logmsg);
 
-                    pthread_create(&threadid[i++], &attr, &sockthread, (void *) client_fd);
+                    threadParam->sockfd = client_fd;
+                    pthread_create(&threadid[i++], &attr, &serverthread, (void *)threadParam);
                     sleep(0); // Giving threads some CPU time
                 }// end if (FD_ISSET(i, &ready_set))
         }// end switch
     } // end while (status==CONTINUE)
     close(sockfd);
     unlinkPortFile(addrstr);
-    exit(0);
+
+    return 0;
 }
