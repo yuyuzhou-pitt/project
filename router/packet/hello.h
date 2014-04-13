390cff6ee904755582a2f3e52e12c3f58ab02eb7
qguan <qguan@localhost.localdomain>
2014-04-13 00:24:53 -0400

    remove hello thread

--- router/packet/hello.h
+++ router/packet/hello.h
@@ -5,8 +5,7 @@
 #include "packet.h"
 #include "../config/config.h"
 
-Packet *genHelloReq(Router *router);
-void *helloserver(void *arg);
-void *helloclient(void *arg);
+Packet *genHelloReq(Router *router, int port);
+int sendHello(int sockfd, Router *router, int port);
 
 #endif
